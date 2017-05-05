97028ab1b97799cd394db10b8a4d3d22aea4bc32
dpausp <dpausp@posteo.de>
2017-05-05 19:34:39 +0200

    operation basics work
    * based on http://trailblazer.to/guides/trailblazer/2.0/01-operation-basics.html

---
+++ pyrailway/operation.py
@@ -0,0 +1,76 @@
+class Operation:
+    def __init__(self, *stations):
+        self.stations = stations
+        
+        
+    def __call__(self, params=None, **dependencies):
+        options = dict(params=(params or {}), **dependencies)
+        success = True
+        for station in self.stations:
+            if (success and station.runs_on_success) or (not success and station.runs_on_failure): 
+                success = station(options, dependencies)
+            
+        return Result(success, options)
+
+
+class Result:
+    def __init__(self, success, result_data):
+        self.result_data = result_data
+        self.success = success
+        
+    @property
+    def failure(self):
+        return not self.success
+    
+    def __getitem__(self, key):
+        return self.result_data[key]
+    
+    def __contains__(self, key):
+        return key in self.result_data
+    
+    def get(self, key):
+        return self.result_data.get(key)
+    
+    
+
+class Activity:
+    
+    runs_on_success = False
+    runs_on_failure = False
+    
+    def __init__(self, func):
+        self.func = func
+        
+    def callfunc(self, options, dependencies):
+        params = options["params"]
+        return self.func(options=options, params=params, **dependencies)
+    
+    def __call__(self, options, dependencies):
+        self.callfunc(options, dependencies)
+        return True
+
+    def __repr__(self):
+        return "{} with {}".format(self.__class__.__name__, self.func.__name__)
+    
+
+class step(Activity):
+    
+    runs_on_success = True
+
+    def __call__(self, options, dependencies):
+        res = self.callfunc(options, dependencies)
+        return bool(res)
+
+
+class failure(Activity):
+
+    runs_on_failure = True
+    
+    def __call__(self, options, dependencies):
+        self.callfunc(options, dependencies)
+        return False
+        
+
+class success(Activity):
+
+    runs_on_success = True
---
+++ pyrailway/tests/test_operation.py
@@ -0,0 +1,100 @@
+from funcy import rcompose
+from pyrailway.operation import success, failure, step, Operation
+
+
+ENJOY_MSG = "great to hear, enjoy your day"
+JOKE = "Broken pencils are pointless"
+
+
+def hello(options, **k):
+    print("hello")
+    return True
+
+
+def fail(options, **k):
+    return False
+    
+
+def world(options, **k):
+    print("world")
+    return True
+
+
+hello_world = rcompose(hello, world)
+
+
+def how_are_you(options, params, **k):
+    return params["happy"] == "yes"
+
+
+def enjoy_your_day(options, **k):
+    print(ENJOY_MSG)
+    
+
+def tell_joke(options, **k):
+    options["joke"] = JOKE
+    print(JOKE)
+    
+    
+def test_step_function_args():
+    def func(options, params, dep):
+        assert options["params"] == params
+        assert options["dep"] == dep
+        
+    op = Operation(step(func))
+    op({"happy": "yes"}, dep=5)
+
+
+def test_one_step(capsys):
+    
+    create = Operation(step(hello))
+    result = create()
+
+    out, _ = capsys.readouterr()
+    assert out == "hello\n"
+    
+    assert result.success
+    
+    
+def test_fail_step():
+    op = Operation(step(fail))
+    res = op()
+    assert res.failure
+    
+    
+def test_success_step():
+    op = Operation(success(fail))
+    res = op()
+    assert res.success
+    
+
+def test_two_steps(capsys):
+    op = Operation(step(hello), step(world))
+    result = op()
+
+    out, _ = capsys.readouterr()
+    assert out == "hello\nworld\n"
+    
+    assert result.success
+
+
+def test_input_success(capsys):
+    op = Operation(step(how_are_you), success(enjoy_your_day))
+    res = op({"happy": "yes"})
+    out, _ = capsys.readouterr()
+    assert out == ENJOY_MSG + "\n"
+    assert res.success
+
+
+def test_input_failure():
+    op = Operation(step(how_are_you), success(enjoy_your_day))
+    res = op({"happy": "I am sad"})
+    assert res.failure
+    
+    
+def test_with_failure_handler():
+    op = Operation(step(how_are_you), success(enjoy_your_day), failure(tell_joke))
+    res = op({"happy": "I am sad"})
+    assert res.failure
+    assert "joke" in res
+    assert res["joke"] == JOKE
